275f1f86d0786ae4bf1864850289e7777a4257f0
CSO <Chr.sorensen1990@gmail.com>
2015-12-07 17:48:20 +0100

    stuff

--- ITTWEB-Opgave2-Protein/ITTWEB-Opgave2-Protein/Assets/app/controllers/newProteinCtrl.js
+++ ITTWEB-Opgave2-Protein/ITTWEB-Opgave2-Protein/Assets/app/controllers/newProteinCtrl.js
@@ -12,19 +12,40 @@
                     });
             };
 
-            function calcProtein(data) {
+            var calcProtein = function(data) {
                 for (var i = 0, len = data.length; i < len; i++) {
                     data[i].Protein = (data[i].Amount) * (data[i].FoodPosibility.ProteinRatio);
                 }
                 return data;
             }
-
+            var getFoodPosibilities = function () {
+                $http.get("/api/WsProtein/GetFoodPosibilities")
+                    .success(function (data, status, headers, config) {
+                        $scope.foodPosibilities = data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        console.log(data);
+                    });
+            };
+            
             $scope.deleteRow = function (index) {
                 var deleteId = $scope.foodIntakeData[index].Id;
                 console.log(deleteId);
                 $http.post("/api/WsProtein/DeleteFoodIntake", deleteId)
                     .success(function(data, status, headers, config) {
                         $scope.getList();
+                    });
+            };
+
+            $scope.updateRow = function (index) {             
+                var update = $scope.foodIntakeData[index];  
+                console.log(update);
+                $http.post("/api/WsProtein/PostFoodIntake", update)
+                    .success(function (data, status, headers, config) {
+                        $scope.getList();
+                    })
+                    .error(function (data, status, headers, config) {
+                        console.log(data);
                     });
             };
 
